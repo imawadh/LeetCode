1767cb5cf05c80eea14286f5aa5094fee55bb3f1
Awadh Kishor Singh <awadhkishorsingh241@gmail.com>
2024-05-07 22:19:47 +0530

    Types of Triangle

---
+++ types_of_triangle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        if(nums[0]+nums[1]>nums[2] && nums[0]+nums[2]>nums[1] && nums[1]+nums[2]>nums[0])
+        {
+        if(nums[0]==nums[1] && nums[2]==nums[1])
+        {
+            return "equilateral";
+        }
+        else if ((nums[0]==nums[1] && nums[2]!=nums[1]) || (nums[0]!=nums[1] && nums[2]==nums[1]) || (nums[0]==nums[2] && nums[2]!=nums[1]))
+        {
+            return "isosceles";
+        }
+        else{
+            return "scalene";
+        }
+        }
+        else{
+            return "none";
+        }
+    }
+};
