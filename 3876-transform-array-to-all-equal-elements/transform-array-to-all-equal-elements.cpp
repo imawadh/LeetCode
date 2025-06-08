6eb26ab8746b817cacc185f2cac28c82f5a181dd
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-06-08 11:44:47 +0530

    Time: 18 ms (35.80%) | Memory: 158.3 MB (66.28%) - LeetSync

--- 3876-transform-array-to-all-equal-elements/transform-array-to-all-equal-elements.cpp
+++ 3876-transform-array-to-all-equal-elements/transform-array-to-all-equal-elements.cpp
@@ -1,48 +1,28 @@
 class Solution {
 public:
     bool canMakeEqual(vector<int>& nums, int k) {
-        vector<int> v = nums;
-        int m = k;
+        vector<int> temp = nums;
         int n = nums.size();
+        int cnt = 0;
         for(int i =0; i<n-1; i++){
-            if(k<=0){
-                break;
+            if(temp[i]==-1){
+                temp[i+1]*=-1;
+                cnt++;
             }
-            if(nums[i]==-1){
-                nums[i]*=-1;
-                nums[i+1]*=-1;
-                k--;
+        }
+        if(cnt<=k && temp[n-1]==1) return true;
+        
+        temp = nums;
+        cnt = 0;
+        for(int i =0; i<n-1; i++){
+            if(temp[i]==1){
+                temp[i+1]*=-1;
+                cnt++;
             }
+        }
+        if(cnt<=k && temp[n-1]==-1) return true;
 
-        }
-        for(int i =0; i<n-1; i++){
-            if(m<=0){
-                break;
-            }
-            if(v[i]==1){
-                v[i+1]*=-1;
-                v[i]*=-1;
-                m--;
-            }
-        }
-        
-        bool f1 = true;
-        bool f2 = true;
-        for(int i =0; i<n; i++){
-            if(nums[i]==-1){
-                f1 = false;
-                break;
-            }
-        }
-        for(int i =0; i<n; i++){
-            if(v[i]==1){
-                f2 = false;
-                break;
-            }
-        }
-        return f1||f2;
-        
-        
-       
+        return false;
+    
     }
 };
