50c1eff1f06404520995b02db9d4581daeae42ac
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-06-09 11:16:01 +0530

    Time: 2 ms (53.92%) | Memory: 14.1 MB (54.02%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -5,14 +5,17 @@ public:
         int val = 1;
         for(int i = 0; i<n; i++){
             ans.push_back(val);
+            // This is for moving downward 
             if (val * 10 <= n) {
                 val *= 10;  
             } 
             else {
+                // If value exceeds the limit 
                 if (val >= n) {
                     val /= 10; 
                 }
                 val++;
+                // If bring the number to its lowest form the go further 
                 while (val % 10 == 0) {
                     val /= 10;  
                 }
