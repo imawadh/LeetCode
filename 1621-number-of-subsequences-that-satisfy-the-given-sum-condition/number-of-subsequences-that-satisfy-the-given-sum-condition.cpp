9f7c54e03a05c0ec719a0d046e5a1823f063aefe
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-06-29 09:40:54 +0530

    Time: 59 ms (6.82%) | Memory: 51.6 MB (87.34%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,45 +1,45 @@
 class Solution {
+
 public:
     const long long Mod = 1e9 + 7;
-
-    long long power(int pow) {
-        if(pow < 0) return 0;
-        if(pow == 0) return 1;
-        long long res = power(pow / 2);
+    long long power(int pow){
+        if(pow<0) return 0;
+        if(pow==0) return 1;
+        long long res = power(pow/2);
         res = (res * res) % Mod;
         if(pow & 1) {
             res = (res * 2) % Mod;
         }
         return res;
     }
+    int maxIndex(int st, vector<int>&nums,int target){
+            int ed = nums.size()-1;
+            int res = st-1;
+            target = target - nums[st];
+            while(st<=ed){
+                int mid = (ed-st)/2 + st;
+                if(nums[mid]<=target){
+                    res = mid;
+                    st = mid+1;
+                } 
+                else{
+                    ed = mid-1;
+                }
+            } 
+            return res;
 
-    int maxIndex(int st, vector<int>& nums, int target) {
-        int left = st, right = nums.size() - 1;
-        int res = st - 1;
-        int adjustedTarget = target - nums[st];
-        
-        while(left <= right) {
-            int mid = left + (right - left) / 2;
-            if(nums[mid] <= adjustedTarget) {
-                res = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return res;
     }
-
     int numSubseq(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(),nums.end());
         long long ans = 0;
-        for(int i = 0; i < (int)nums.size(); i++) {
-            int j = maxIndex(i, nums, target);
-            int pow = j - i;
-            if(pow >= 0) {
-                long long val = power(pow);
-                ans = (ans + val) % Mod;
+        for(int i = 0; i<nums.size(); i++){
+            // Applying binary Search to get the index in log N time 
+            int pow = maxIndex(i,nums,target);
+            if(pow>=0){
+                long long val = power(pow-i);
+                 ans = (ans + val) % Mod;
             }
+
         }
         return (int)ans;
     }
