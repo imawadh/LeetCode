64034dd712160abfd24c3d0758b245b4666a9db9
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-07-01 21:58:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (64.63%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
         sort(deck.begin(),deck.end());
-        int idx = 0;
+        int idx = 0; // Sorted Array ke elements ko track akne ke liye 
+
         int n =  deck.size();
         queue<int> q;
         for(int i =0; i<n; i++){
@@ -10,12 +11,15 @@ public:
         }
         vector<int> ans(n);
         for(int i = 0; i<n; i++){
+            // Index Position to put an Element of Sorted Array of idx index ...
             int idxEle = q.front();
             q.pop();
+
+            // Index Position to skip an Element of Sorted Array of idx index ...
             int idxSkip = q.front();
             q.pop();
 
-
+            // Putting sorted array ke elements ko at dwara bataye gaye index position jaha rakhna tha 
             ans[idxEle] = deck[idx];
             idx++;
 
