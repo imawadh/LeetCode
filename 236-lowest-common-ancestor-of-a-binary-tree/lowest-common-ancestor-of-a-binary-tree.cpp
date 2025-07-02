2c11b2c670dcca66c8002189c425d001d4c3ddb4
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-07-02 21:28:11 +0530

    Time: 7 ms (93.19%) | Memory: 18.7 MB (12.36%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void path(TreeNode* root, vector<TreeNode*> &v, TreeNode* Search){
+        if(root == nullptr){
+            return;
+        }
+        v.push_back(root);
+        if(root==Search){
+            return;
+        }
+        path(root->left,v,Search);
+        if(v.back()==Search) return;
+        path(root->right,v,Search);
+        if(v.back()==Search) return;
+
+        v.pop_back();
+        
+    }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==p || root==q) return root;
+        vector<TreeNode*> pV;
+        vector<TreeNode*> qV;
+
+        path(root,pV,p);
+        path(root,qV,q);
+        int i = 0;
+        int n = min(qV.size(),pV.size());
+        // for(int i =0; i<n; i++){
+        //     if(pV[i]!=qV[i]){
+        //         return pV[i];
+        //     }
+        // }
+        // return pV[n-1];
+        while (i < n && pV[i] == qV[i]) {
+            i++;
+        }
+
+        return pV[i - 1]; 
+
+    }
+};
