6945e7d9d4599b41e9ef4668b967f0a4845b337b
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-07-03 17:02:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (64.92%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     char kthCharacter(int k) {
+        /* At most
+        A se k tak hi ke saare charcaters possible hai kewal ...
+        1 a
+        2 b
+        4 c
+        5 d
+        8 e
+        16 f
+        32 g
+        64 h
+        128 i
+        256 j 
+        512 k
+        */ 
         string str = "a";
         while(str.size()<k){
             string s = str;
