00dcf1463b1762a33c5d173f225f7e9403ed7541
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-07-18 00:02:32 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (78.10%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,29 +1,72 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        set<pair<int,int>> st;
+        bool rowFlag = false;
+        bool colFlag = false;
+
         int n = matrix.size();
         int m = matrix[0].size();
+
+        // Col Flag
         for(int i =0; i<n; i++){
-            for(int j =0; j<m; j++){
+            if(matrix[i][0]==0){
+                colFlag = true;
+                break;
+            }
+        }
+
+        // Row flag
+        for(int i =0; i<m; i++){
+            if(matrix[0][i]==0){
+                rowFlag = true;
+                break;
+            }
+        }
+
+
+        for(int i = 1; i<n; i++){
+            for(int j= 1; j<m; j++){
                 if(matrix[i][j]==0){
-                    st.insert(make_pair(i,j));
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for(auto &it: st){
-            cout<<it.first<<' '<<it.second<<'\n';
-            int r = it.first;
-            int c = it.second;
-            // Making rth row 
-            for(int i = 0; i<m; i++){
-                matrix[r][i] = 0;
-            }
-            //making cth column zero
-            for(int i = 0; i<n; i++){
-                matrix[i][c] = 0;
+
+        // Row Zeroing 
+        for(int i = 1; i<n; i++){
+            if(matrix[i][0]==0){
+                for(int j= 1; j<m; j++){
+                    matrix[i][j]=0;
+                }
             }
         }
+        
+        // Column Zeroing 
+        for(int i = 1; i<m; i++){
+            if(matrix[0][i]==0){
+                for(int j= 1; j<n; j++){
+                    matrix[j][i]=0;
+                }
+            }
+        }
+
+        // First row zeroing
+        if(rowFlag){
+            for(int i =0; i<m; i++){
+                matrix[0][i]=0;
+            }   
+        }
+
+        // First col zeroing
+        if(colFlag){
+            for(int i =0; i<n; i++){
+                matrix[i][0]=0;
+            }
+        }
+
+
+
 
     }
 };
