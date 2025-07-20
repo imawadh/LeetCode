519785fb5ffad65b143fbed2b59aa9dc1fce53c6
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-07-20 22:56:28 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (33.06%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -3,14 +3,25 @@ public:
     void nextPermutation(vector<int>& nums) {
         
         int n = nums.size();
+
         for(int i = n-2; i>=0; i--){
+            // find the find place where nums[i]<nums[i+1] for array to be increasing 
             if(nums[i]<nums[i+1]){
                 int j = i+1;
+                // finding the nearest greater integer 
                 while(j<n && nums[j]>nums[i]){
                     j++;
                 }
+                // swap and revere the aray now .... 
                 swap(nums[j-1],nums[i]);
-                reverse(nums.begin()+i+1, nums.end());
+                // reverse(nums.begin()+i+1, nums.end());
+                i = i+1;
+                j = n-1;
+                while(i<j){
+                    swap(nums[i],nums[j]);
+                    i++;
+                    j--;
+                }
                 return;
             }
         }
