f83f0a83f59d170e866e0bb136a238c2b582aaf5
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-07-28 07:58:06 +0530

    Time: 31 ms (43.84%) | Memory: 11.1 MB (35.87%) - LeetSync

---
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int ct = 0;
+    int maxOr = 0;
+    void generateSubset(vector<int>&nums, vector<int>&v, int idx){
+        if(idx>=nums.size()){
+            int currOr = 0;
+            for(int i =0; i<v.size(); i++){
+                currOr|=v[i];
+            }
+            if(currOr==maxOr){
+                ct++;
+            }else if(maxOr<currOr){
+                maxOr = currOr;
+                ct = 1;
+            }
+            return;
+        }
+        generateSubset(nums, v, idx+1);
+        v.push_back(nums[idx]);
+        generateSubset(nums, v, idx+1);
+        v.pop_back();
+    }
+    int countMaxOrSubsets(vector<int>& nums) {
+        vector<int> v;
+        generateSubset(nums, v, 0);
+        return ct;
+    }
+};
