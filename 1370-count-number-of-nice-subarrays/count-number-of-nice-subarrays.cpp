6784c9af96187769e807bbc29ad7645edba3b6ef
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-08-10 17:12:27 +0530

    Time: 0 ms (100.00%) | Memory: 71.3 MB (92.92%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -10,53 +10,30 @@ public:
             if(nums[j]&1){
                 ct++;
             }
-            // if(ct==k){
-            //     int r = 1;
-            //     int idx = j+1;
-            //     while(idx<n && ct==k){
-            //         if(!(nums[idx]&1)){
-            //             idx++;
-            //             r++;
-            //         }else{
-            //             ct--;
-            //             break;
-            //         }
-            //     }
-            //     ans+=r;
-            //     int l = 0;
-            //     while(!(nums[i]&1)){
-            //         l++;
-            //         i++;
-            //     }
-            //     if(l==0){
-            //         i++;
-            //     }
-            //     ans+=l*r;
-            //     j = idx-1;
-            //     ct--;
-            // }
-            // j++;
-            if (ct == k) {
-                // count evens to the right
+            if(ct==k){
                 int r = 1;
-                int idx = j + 1;
-                while (idx < n && (nums[idx] & 1) == 0) {
-                    r++;
-                    idx++;
+                int idx = j+1;
+                while(idx<n){
+                    if(!(nums[idx]&1)){
+                        idx++;
+                        r++;
+                    }else{
+                        break;
+                    }
                 }
-
-                // count evens to the left
-                int l = 1;
-                while ((nums[i] & 1) == 0) {
+                ans+=r;
+                int l = 0;
+                while(!(nums[i]&1)){
                     l++;
                     i++;
                 }
-
-                ans += l * r; // all combinations
-                i++;          // move past first odd
-                ct--;         // one odd removed from window
+                i++;
+                ans+=l*r;
+                j = idx-1;
+                ct--;
             }
             j++;
+            
         }     
         return ans;
     }
