33bf668c1468b91a3c45f0ddb215899a1c19dbc1
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-08-12 14:21:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (54.51%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+    
+        vector<char> visitedWithColor(n,'.');
+
+        for(int i = 0; i<n; i++){
+            if(visitedWithColor[i]=='.'){
+                queue<int> q;
+                q.push(i);
+
+                visitedWithColor[i] = 'R';
+                char color = 'R';
+
+                while(q.size()){
+                    int edge = q.front();
+                    q.pop();
+                    color = (visitedWithColor[edge]=='R') ? 'B' : 'R';
+                    for(auto &it:graph[edge]){
+                        if(visitedWithColor[edge] == visitedWithColor[it]){
+                            return false;
+                        }
+
+                        if(visitedWithColor[it]=='.'){
+                            q.push(it);
+                            visitedWithColor[it] = color;
+                            
+                        }
+                        
+                    }
+                   
+                }
+            }
+        }
+        return true;
+          
+    }
+};
