615e8c6dce9d3ad7cc88b194d013f5b55294b7df
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-10-18 17:34:32 +0530

    Time: 11 ms (76.71%) | Memory: 88.8 MB (27.78%) - LeetSync

--- 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.cpp
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-// Find Next index on the right whose value is greater
     vector<int> canSeePersonsCount(vector<int>& heights) {
+        // Monotonic decresing stack
+        stack<int> st;
         int n = heights.size();
         vector<int> ans(n,0);
-        stack<int> st;
+        ans[n-1]= 0;
         st.push(heights[n-1]);
-        for(int i = n-2; i>=0 ;i--){
+        for(int i = n-2; i>=0; i--){
             int ct = 0;
-            while(st.size() && st.top()<=heights[i]){
-                ct++;
+            while(st.size() && heights[i]>st.top()){
                 st.pop();
+                ct++;
             }
-            if(st.size()) ct++;
-            ans[i] = ct;
+            if(st.empty()) ans[i] =ct;
+            else{
+                ans[i] = ct+1;
+            }
+            
             st.push(heights[i]);
-        }   
+        }
         return ans;
     }
 };
