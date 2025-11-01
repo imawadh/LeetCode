cfce5802da9e7e6dfc993a74a5874d0dc7273252
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-11-01 06:08:34 +0530

    Time: 2653 ms (5.12%) | Memory: 266 MB (24.04%) - LeetSync

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        ListNode* tempHead = new ListNode(INT_MIN);
+        set<int> st;
+        for(int i =0; i<nums.size(); i++){
+            st.insert(nums[i]);
+        }
+        tempHead->next = head;
+        cout<<tempHead->next->val<<'\n';
+        ListNode*  temp = tempHead;
+        cout<<temp->next->val<<'\n';
+
+        while(temp->next!=nullptr){
+            int val = temp->next->val;
+            cout<<val<<" :  ";
+            if(st.find(val)!=st.end()){
+                cout<<"found";
+                temp->next = temp->next->next;
+            }else{
+                cout<<"not found";
+                temp = temp->next;
+            }
+        }
+
+        return tempHead->next;
+    }
+};
