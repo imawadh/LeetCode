c07038016a1aae86c1a4303051055f6c08fa3905
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-11-03 06:06:51 +0530

    Time: 1435 ms (5.08%) | Memory: 125.3 MB (5.08%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -4,28 +4,22 @@ public:
         int n = colors.size();
         int i = 0;
         int ans = 0;
-
-        while (i < n) {
+        while(i<n){
             vector<int> temp;
             int j = i;
-
-            // collect all times for same-color group
-            while (j < n && colors[j] == colors[i]) {
+            while(j<n && colors[j]==colors[i]){
                 temp.push_back(neededTime[j]);
                 j++;
             }
-
-            // remove all except the largest time
-            sort(temp.begin(), temp.end());
-            for (int k = 0; k < (int)temp.size() - 1; k++) {
-                ans += temp[k];
-                // cout << temp[k] << ' '; // (debug optional)
+            // temp.push_back(0);
+            sort(temp.begin(),temp.end());
+            for(int k = 0; k<temp.size()-1;k++){
+                ans+=temp[k];
+                cout<<temp[k]<<' ';
             }
-            // cout << '\n'; // (debug optional)
-
-            i = j; // move to next group
-        }
-
+            cout<<'\n';
+            i = j;
+        }   
         return ans;
     }
 };
