7cfaee95a6c71afa9d4e15aade202f7b98b01305
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2025-11-05 05:55:15 +0530

    Time: 3 ms (42.33%) | Memory: 20.1 MB (50.21%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
+    vector<vector<int>> adj;
     vector<bool> vis;
     int n;
 
-    void dfs(vector<vector<int>>& isConnected, int node) {
+    void dfs(int node) {
         vis[node] = true;
-        for (int j = 0; j < n; j++) {
-            if (isConnected[node][j] == 1 && !vis[j]) {
-                dfs(isConnected, j);
+        for (int neighbor : adj[node]) {
+            if (!vis[neighbor]) {
+                dfs(neighbor);
             }
         }
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
         n = isConnected.size();
+        adj.assign(n, {});
         vis.assign(n, false);
-        int provinces = 0;
 
+        // ✅ Step 1: Build adjacency list from the adjacency matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1 && i != j) {
+                    adj[i].push_back(j);
+                }
+            }
+        }
+
+        // ✅ Step 2: DFS to count connected components
+        int provinces = 0;
         for (int i = 0; i < n; i++) {
             if (!vis[i]) {
                 provinces++;
-                dfs(isConnected, i);
+                dfs(i);
             }
         }
 
