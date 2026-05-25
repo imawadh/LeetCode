72849f3c658c18e20cffaa4a84da6787798d4897
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2026-05-25 13:17:34 +0530

    Time: 810 ms (5.01%) | Memory: 52.1 MB (98.35%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        bool ans = false;
+        int i = 0;
+        int maxCurr = 0;
+        for(i = 0; i<nums.size(); i++){
+            if(maxCurr>=i){
+                maxCurr = max(maxCurr,nums[i]+i);
+            }
+            cout<<maxCurr<<' ';
+
+        }
+        if(maxCurr>=nums.size()-1){
+            ans = true;
+        }
+        return ans;
+
+    }
+};
